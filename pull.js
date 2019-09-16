1225f487448ab14a59252a24f06d152b88f89719
Andrea Bender <andreab.1726@gmail.com>
2019-09-16 15:09:07 -0600

    try catch

--- pull.js
+++ pull.js
@@ -1,4 +1,7 @@
 function getYearOfBirth(age) {
+    if (age < 0) {
+    throw new Error ("Age cannot be negative");
+    }
 
     const yearOfBirth = 2019 - age;
     return yearOfBirth;
@@ -9,5 +12,10 @@ function createGreeting(name, age) {
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${getYearOfBirth(age)}`;
 }
 
-const  greeting1 = createGreeting("Chris", "29");
+try {
+const  greeting1 = createGreeting("Chris", "29");}
+catch (Error) {
+    console.log (error.message);
+}
+
 console.log(greeting1);
